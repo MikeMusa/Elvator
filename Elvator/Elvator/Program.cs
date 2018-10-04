2c8197750598b4cb30327c81fc2ef1c5437dadc2
mikemusa90 <43837516+mikemusa90@users.noreply.github.com>
2018-10-03 20:46:40 -0700

    Add files via upload

---
+++ Elvator/Elvator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Elvator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ElvaPerson;
+            int Elvalimit=8;
+            Console.WriteLine("Enter Value?");
+
+            ElvaPerson = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You have entered = " + ElvaPerson);
+
+            if (ElvaPerson <= Elvalimit)
+            {
+                Console.WriteLine("Sucess");
+            }
+            else
+            {
+                Console.WriteLine("Exceeded allowed number of people");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
